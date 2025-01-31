7284ab94159f65ff6a1dd6d8da266ea3d5eca44a
Server TATI <server-tati>
2025-01-31 15:27:31 +0700

    business category, meeting room

---
+++ internal_tati/internal_tati/doctype/bussiness_category/bussiness_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, tati and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bussiness Category', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ internal_tati/internal_tati/doctype/meeting_room_schedule/meeting_room_schedule.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, tati and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Meeting Room Schedule', {
+	// refresh: function(frm) {
+
+	// }
+});
